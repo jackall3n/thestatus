1091d09bf4774b161404a5000f476d51d2f780f0
Jack Allen <jackallensemail+bit@googlemail.com>
2019-02-16 23:18:55 +0000

    update

--- src/server.js
+++ src/server.js
@@ -31,7 +31,9 @@ app.get('/services', (req, res) => {
 
         statuses.forEach((status, index) => {
             const service = services[index];
-            response[service] = status;
+            response[service] = {
+                status
+            };
         });
 
         res.send(response);
