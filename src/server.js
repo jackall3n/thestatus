924f95b3b6e966a082f6857f88fceba41cf2905d
Jack Allen <jackallensemail+bit@googlemail.com>
2019-02-16 23:08:38 +0000

    scripts

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
 const express = require('express');
 const axios = require('axios');
 
@@ -18,6 +21,15 @@ app.get('/ip', (req, res) => {
         res.send(response.data);
     })
 });
+
+app.get('services', (req, res) => {
+    async function ls() {
+        const { stdout, stderr } = await exec('ls');
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+    }
+    ls();
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
