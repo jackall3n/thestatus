c5143a2183c089f61aa87b9b69f533979d62b127
Jack Allen <jackallensemail+bit@googlemail.com>
2019-02-16 23:58:59 +0000

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 service=thestatus.service
+service_dir=/etc/systemd/system
 
-sudo cp ${service} /etc/systemd/system
+echo "copying ${service} to ${service_dir}"
+sudo cp ${service} ${service_dir}
+echo "copied ${service}"
 
+echo "enabling ${service}"
 sudo systemctl enable ${service}
+echo "enabled ${service}"
 
+echo "starting ${service}"
 sudo systemctl start ${service}
+echo "started ${service}?"
